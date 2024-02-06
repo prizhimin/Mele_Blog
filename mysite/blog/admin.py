ab3254f3fea8bd6f61103d5b706c565d2b780bfd
prizhimin <prinv@yandex.ru>
2024-02-06 10:19:46 +0500

    Глава 1 завершена

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -2,4 +2,12 @@ from django.contrib import admin
 from .models import Post
 
 # Register your models here.
-admin.site.register(Post)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug', 'author', 'publish', 'status']  # поля для показа на админской панели
+    list_filter = ['status', 'created', 'publish', 'author']
+    search_fields = ['title', 'body']  # строка поиска - по каким полям искать
+    prepopulated_fields = {'slug': ('title',)}
+    # raw_id_fields = ['author']
+    date_hierarchy = 'publish'
+    ordering = ['status', 'publish']
