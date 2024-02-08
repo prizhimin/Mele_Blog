f05d17d72652701d6cd8e1d748cd1e1209ebcc6a
prizhimin <prinv@yandex.ru>
2024-02-08 08:38:32 +0500

    Разбивка на страницы

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -5,10 +5,11 @@ from django.core.paginator import Paginator
 
 
 def post_list(request):
-    posts = Post.published.all()
+    post_list = Post.published.all()
     # Постраничная разбивка с 3 постами на страницу
     paginator = Paginator(post_list, 3)
     page_number = request.GET.get('page', 1)
+    posts = paginator.page(page_number)
     return render(request,
                   'blog/post/list.html',
                   {'posts': posts})
