c6fb24ff5abab6609ab50311b1d12f76eb6682b2
prizhimin <prinv@yandex.ru>
2024-02-13 17:01:50 +0500

    Усовершенствование блога за счет продвинутых функциональностей (стр. 130)

--- mysite/blog/forms.py
+++ mysite/blog/forms.py
@@ -2,11 +2,12 @@ from django import forms
 from .models import Comment
 
 class EmailPostForm(forms.Form):
-    name = forms.CharField(max_length=25)
+    name = forms.CharField(max_length=35, label='Имя')
     email = forms.EmailField()
     to = forms.EmailField()
     comments = forms.CharField(required=False,
-                               widget=forms.Textarea)
+                               widget=forms.Textarea,
+                               label='Комментарий')
 
 
 class CommentForm(forms.ModelForm):
