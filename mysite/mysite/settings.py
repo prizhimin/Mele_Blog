c8aa94e2b5aaadc75e851957b8483d4495e6ecc2
prizhimin <prinv@yandex.ru>
2024-02-15 14:31:12 +0500

    PostgreSQL (стр. 172)

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -80,8 +80,13 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blog',
+        'USER': 'blog',
+        'PASSWORD': 'hjenth2213',
+        'HOST': '10.168.11.206'
     }
 }
 
